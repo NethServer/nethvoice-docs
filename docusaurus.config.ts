14c240f08c9f4d7fab3db2b1194e61e1ead4d7f0
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2026-06-29 08:52:22 +0200

    feat: add PR preview

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -56,9 +56,9 @@ const config: Config = {
     : 'https://nethserver.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: process.env.NODE_ENV === 'production' 
-    ? '/' 
-    : '/nethvoice-docs/',
+  baseUrl: process.env.BASE_URL ?? (process.env.NODE_ENV === 'production'
+    ? '/'
+    : '/nethvoice-docs/'),
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
